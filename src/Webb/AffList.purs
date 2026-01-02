228620efad6f7fd577f95aa0212bd24891c6d32b
Howard Chen <hchen7913@gmail.com>
2026-01-02 13:40:44 -0600

    Implement the public API for the AffList

--- src/Webb/AffList.purs
+++ src/Webb/AffList.purs
@@ -1,3 +1,16 @@
-module Webb.AffList where
+module Webb.AffList 
+( module P
+
+)
+where
 
 import Prelude
+
+import Webb.AffList.Monad.AffList (AffList, runYieldToList, class LaunchList, launchList) as P
+import Webb.AffList.Monad.Yield (Yield, addParent, finally, onCancel, asAff) as P
+import Webb.AffList.Behavior.List (take, drop, takeWhile, dropWhile, fromFoldable, foldl, scanl, filter, reject, groupN) as P
+import Webb.AffList.Internal.ListFiber (ListFiber, LFiber, kill, receive, receive', forEach_, while_) as P
+
+{-
+The AffList provides all methods needed to work with the AffList.
+-}
