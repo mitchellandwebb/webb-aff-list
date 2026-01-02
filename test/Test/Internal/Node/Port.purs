f50d8f41aacaba154ef3d88d2e19a9c8f1a898f2
Howard Chen <hchen7913@gmail.com>
2026-01-02 15:55:04 -0600

    Test the port internals

--- test/Test/Internal/Node/Port.purs
+++ test/Test/Internal/Node/Port.purs
@@ -1,8 +1,11 @@
 module Test.Internal.Node.Port where
 
-import Prelude
+import Test.Prelude
 
+import Effect.Aff (Aff)
+import Webb.AffList.Internal.Node.Port as Port
 import Webb.AffList.Internal.Node.State as State
+import Webb.Thread as Thread
 
 
 type PortProps = Unit
@@ -10,3 +13,18 @@ type PortProps = Unit
 type State a = State.NodeState a
 
 
+new :: forall a. Aff (State a)
+new = do 
+  state <- State.new
+  pure state
+
+-- Install a program into the port that has access to the state.
+new' :: forall a. (State a -> Aff Unit) -> Aff (State a)
+new' f = do 
+  state <- State.new
+  Thread.setProgram state.thread (f state)
+  pure state
+  
+isOpen :: forall a. State a -> Boolean -> Aff Unit
+isOpen state flag = do 
+  Port.isOpen state ?= flag
--- test/Test/Internal/Node/PortSpec.purs
+++ test/Test/Internal/Node/PortSpec.purs
@@ -2,8 +2,83 @@ module Test.Internal.Node.PortSpec where
 
 import Test.Prelude
 
+import Effect.Exception (error)
+import Test.Internal.Node.Port as Props
+import Test.Spec (before_)
+import Webb.AffList.Data.Node.Port as PortValue
+import Webb.AffList.Internal.Node.Port as Port
+import Webb.Monad.Prelude (delayInt, launch_)
+import Webb.Stateful (localEffect)
+
 
 spec :: Spec Unit
 spec = describe "Port internals" do 
+  before_ reset do
+    it "sending starts the program" do 
+      p <- new' (\port -> send port 2)
+      send p 1
+      receive p
+      
+      delayInt 5
+      receivedIs 1
+      
+      receive p
+      delayInt 5
+      receivedIs 2 -- this value was caused by the initial send starting the program.
+      
+    it "receiving will start the program" do 
+      p <- new' (\port -> send port 1)
+      receive p
+      
+      delayInt 5
+      receivedIs 1 -- We received a 1 because the 'receive' triggered the program.
+      
+    it "submitting an error does not require a second receive to continue" do 
+      p <- new
+      receive p
+      err p "hello"
+      delayInt 5
+      receivedIs (-1000)
+      
+    it "closing works as expected" do 
+      p <- new
+      isOpen p true
+      receive p
+      close p
+      delayInt 5
+      receivedIs (-100)
+      isOpen p false
+    
+  where
+  received = localEffect do newShowRef (-1)
+  reset = (received := -1)
+  receivedIs n = do 
+    aread received ?= n
 
-  pure unit
+  new = Props.new
+  new' = Props.new'
+  
+  isOpen = Props.isOpen
+  
+  err p string = do
+    void $ Port.error p (error string)
+    
+  close p = do 
+    void $ Port.close p
+  
+  send port n = do
+    launch_ do
+      void $ Port.send port n
+      
+  receive port = do 
+    launch_ do 
+      mval <- Port.receive port
+      val <- case mval of 
+        PortValue.Closed -> do 
+          pure (-100)
+        PortValue.Err _ -> do 
+          pure (-1000)
+        PortValue.Value v -> do 
+          pure v
+      received := val
+    
